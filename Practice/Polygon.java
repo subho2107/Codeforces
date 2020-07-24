3c265553c3afb892c39f0a04da0a44f714118397
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2020-07-24 09:16:44 +0530

    update all

--- Practice/Polygon.java
+++ Practice/Polygon.java
@@ -10,6 +10,7 @@ public class Polygon {
             int n = Integer.parseInt(buffer.readLine());
             char [] inp;
             char [][] matrix = new char[n][n];
+            System.out.println("an");
             for(int rowPos = 0; rowPos < n; rowPos++)
             {
                 inp = buffer.readLine().toCharArray();
